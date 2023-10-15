dd4c4a7f14b6d845fb0e7d0a2500f8e9917f48dd
anasyakubu <yakubuanas04@gmail.com>
2023-10-15 18:31:42 +0100

    Added what's On section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HomeHeader from "./sections/HomeHeader/HomeHeader";
+import WhatsOn from "./sections/WhatsOn/WhatsOn";
 
 export default function Home() {
   return (
     <div className="">
       <HomeHeader />
+      <WhatsOn />
     </div>
   );
 }
---
+++ src/app/sections/WhatsOn/WhatsOn.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const WhatsOn = () => {
+  return (
+    <div className="WhatsON">
+      <div className="p-10 space-y-12 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
+        <div className="text-center">
+          <h3 className="text-3xl font-semibold">Whats On</h3>
+          <p className="mt-5 text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+            consequatur. Natus, dignissimos eveniet. Nisi dolores enim aliquam
+            eligendi aperiam laborum dignissimos eaque pariatur, molestiae,
+            assumenda blanditiis sed? Nihil accusamus reprehenderit officiis
+            dolores aliquam itaque. A facilis aspernatur necessitatibus,
+            sapiente saepe aliquid autem iste commodi! Laudantium mollitia
+            distinctio eius sit modi? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eveniet dolores sapiente voluptate. Reprehenderit,
+            mollitia sequi! Iure rem illum sint sunt!
+          </p>
+        </div>
+        <div className="text-center">
+          <div className="mockup-phone">
+            <div className="camera"></div>
+            <div className="display">
+              <div className="artboard artboard-demo phone-1">Hi.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhatsOn;
