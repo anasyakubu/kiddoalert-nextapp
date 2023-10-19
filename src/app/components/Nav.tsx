c719d47f90ce8e2f8b8f74e0e743288e46e33697
anasyakubu <yakubuanas04@gmail.com>
2023-10-19 07:34:39 +0100

    Stories section

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,32 +1,18 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link'
+import React from 'react'
 
 const Nav = () => {
   return (
     <div>
-      <div className="navbar bg-transparent" style={{ color: "#122231" }}>
+      <div className="navbar bg-transparent" style={{ color: '#122231' }}>
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
               </svg>
             </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-white"
-            >
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 bg-white">
               <li>
                 <a>Item 1</a>
               </li>
@@ -49,32 +35,29 @@ const Nav = () => {
           <a className="btn btn-ghost normal-case text-xl">Kiddo Alert</a>
         </div>
 
-        <div
-          className="navbar-center hidden lg:flex"
-          style={{ color: "#122231" }}
-        >
+        <div className="navbar-center hidden lg:flex" style={{ color: '#122231' }}>
           <ul className="menu menu-horizontal px-1">
             <li>
               <Link href="/">Home</Link>
             </li>
             <li>
-              <a>About</a>
+              <Link href="/">About</Link>
             </li>
             <li tabIndex={0}>
               <details>
                 <summary>Parent</summary>
                 <ul className="p-2 bg-white">
                   <li>
-                    <a>Submenu 1</a>
+                    <Link href="/">Submenu 1</Link>
                   </li>
                   <li>
-                    <a>Submenu 2</a>
+                    <Link href="/">Submenu 2</Link>
                   </li>
                 </ul>
               </details>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link href="/stories">Stories</Link>
             </li>
           </ul>
         </div>
@@ -83,7 +66,7 @@ const Nav = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
---
+++ src/app/stories/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StoriesHeader from '../sections/StoriesHeader/StoriesHeader'
+
+const page = () => {
+  return (
+    <div className="page">
+      <StoriesHeader />
+    </div>
+  )
+}
+
+export default page
